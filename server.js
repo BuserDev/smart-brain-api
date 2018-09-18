3fa279382ba4248e8541c1157c0a05c22643bba0
Robert Buser <robertbuser@icloud.com>
2018-09-18 08:56:29 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex =  require('knex');
+
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res)=>{
+})
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleGetProfile(db))
+app.put('/image', image.handleImage(db))
+app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)})
+
+app.listen(3000, () => {})
